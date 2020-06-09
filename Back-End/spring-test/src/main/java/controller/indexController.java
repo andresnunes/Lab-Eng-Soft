cf58e0581e8726dda752852c00e4b0c8bbe01fe3
Stella Blima <64937243+stellablima@users.noreply.github.com>
2020-06-09 14:24:07 -0300

    criando nova branch

---
+++ Back-End/spring-test/src/main/java/controller/indexController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+    
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
