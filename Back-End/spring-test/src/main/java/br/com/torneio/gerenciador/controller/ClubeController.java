23469ca199b40f3138fe44edaae88ea71abdcb59
Stella Blima <64937243+stellablima@users.noreply.github.com>
2020-07-08 22:22:26 -0300

    ClubeController - REST

--- Back-End/spring-test/src/main/java/br/com/torneio/gerenciador/controller/ClubeController.java
+++ Back-End/spring-test/src/main/java/br/com/torneio/gerenciador/controller/ClubeController.java
@@ -1,36 +1,23 @@
 package br.com.torneio.gerenciador.controller;
 
-import java.net.URI;
+
 import java.util.List;
 import java.util.Optional;
-
-import javax.transaction.Transactional;
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import br.com.torneio.gerenciador.dto.ClubeDto;
-import br.com.torneio.gerenciador.form.AtualizaClubeForm;
-import br.com.torneio.gerenciador.form.ClubeForm;
 import br.com.torneio.gerenciador.model.Clube;
 import br.com.torneio.gerenciador.model.Organizador;
+import br.com.torneio.gerenciador.model.Torneio;
 import br.com.torneio.gerenciador.repository.ClubeRepository;
 import br.com.torneio.gerenciador.repository.OrganizadorRepository;
-
 
 @Controller
 @RequestMapping("/clube")
@@ -41,68 +28,56 @@ public class ClubeController {
 	private OrganizadorRepository or;
 	
 	@RequestMapping("/signup")
-	public String signup() { return("formClube");}
-	
+	public String signup() { 
+		return "formClube";
+	}
 	@PostMapping("/signup") 
-	public String saveClube(@Valid Clube clube, @Valid Organizador organizador, BindingResult result, RedirectAttributes attributes){
-		if(result.hasErrors()){
-            attributes.addFlashAttribute("mensagem", "Verifique os campos");
-            System.out.println("Verifique os campos");
+	public String saveClube(@Valid Clube clube, BindingResult result, RedirectAttributes attributes, @Valid Organizador organizador, BindingResult result2, RedirectAttributes attributes2){
+		if(result.hasErrors() || result2.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique os campos"); //!
             return "redirect:/clube/signup";
         }
-		cr.save(clube);	
-//FURURAMENTE VERIFICAR A PERSISTENCIA SEGURA DOS DADOS PRA HOJE, OK
-		organizador.setClube(clube);
-		or.save(organizador);	
+		saveClubeService(clube, organizador);	
 		return "redirect:/"+ organizador.getId() +"/torneio/view";
 	}
-	/*
-	@GetMapping
-	public List<ClubeDto> listaClube(String nomeClube){
-		if (nomeClube == null) {
-			List<Clube> Clube = clubeRepository.findAll();
-			return ClubeDto.converter(Clube);
-		} else {
-			List<Clube> Clube = clubeRepository.findByNome(nomeClube);
-			return ClubeDto.converter(Clube);
-		}
+	@ResponseBody
+	private void saveClubeService(Clube clube, Organizador organizador) {
+		cr.save(clube);	
+		organizador.setClube(clube);
+		or.save(organizador);
 	}
-	
-	@PostMapping
-	@Transactional
-	public ResponseEntity<ClubeDto> cadastraClube(@RequestBody @Validated ClubeForm form, UriComponentsBuilder uriBuilder) {
-		Clube clube = form.converter(clubeRepository);
-		clubeRepository.save(clube);
+	@RequestMapping("/{id_organizador}/editar")
+	public ModelAndView formClube(@PathVariable("id_organizador") long id_organizador){
+		ModelAndView mv = new ModelAndView("Clube");
+		Optional<Organizador> organizador = or.findById(id_organizador);
+		mv.addObject("organizador",organizador);
+		Clube clube = organizador.get().getClube();
+		mv.addObject("clube",clube);
 		
-		URI uri = uriBuilder.path("/Clube/{id}").buildAndExpand(clube.getId()).toUri();
-		return ResponseEntity.created(uri).body(new ClubeDto(clube));
-		
+		List<Torneio> torneios = clube.getTorneios();
+		mv.addObject("torneios", torneios);
+		return mv;	
 	}
 	
-	@PutMapping("/{id}")
-	@Transactional
-	public ResponseEntity<ClubeDto> atualizaClube(@PathVariable Long id, @RequestBody  AtualizaClubeForm form) {
-		Optional<Clube> optional = clubeRepository.findById(id);
-		if (optional.isPresent()) {
-			Clube Clube = form.atualizar(id, clubeRepository);
-			return ResponseEntity.ok(new ClubeDto(Clube));
-		}
-		else {
-			return ResponseEntity.notFound().build();
-		}
+	@RequestMapping("/{id_organizador}/editar/{id_torneio}")
+	public String formClubeR(@PathVariable("id_organizador") long id_organizador, @PathVariable long id_torneio) {
+		return"redirect:/{id_organizador}/torneio/editar/"+id_torneio;		
 	}
 	
-	@DeleteMapping("/{id}")
-	@Transactional
-	public ResponseEntity<?> deletaClube(@PathVariable Long id) {
-		Optional<Clube> optional = clubeRepository.findById(id);
-		if (optional.isPresent()) {
-			clubeRepository.deleteById(id);
-			return ResponseEntity.ok().build();
-		}
-		else {
-			return ResponseEntity.notFound().build();
-		}
+	@PostMapping("/{id_organizador}/editar")
+	public String updateClube(@PathVariable("id_organizador") long id_organizador, @Valid Clube clube, BindingResult result, RedirectAttributes attributes, @Valid Organizador organizador, BindingResult result2, RedirectAttributes attributes2){
+		if(result.hasErrors() || result2.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique os campos"); //!
+            return "redirect:/{id_organizador}/editar";
+        }
+		updateClubeService(clube, organizador);	
+		return "redirect:/"+ id_organizador +"/torneio/view";
 	}
-	*/
+	@ResponseBody 
+	private void updateClubeService(Clube clube, Organizador organizador) {
+		//Organizador organizadorUpdate = or.findById(organizador.getId()); //TIRAR OPTIONAL
+		cr.save(clube);	
+		//or.save(organizador);
+	}
 }
+	
