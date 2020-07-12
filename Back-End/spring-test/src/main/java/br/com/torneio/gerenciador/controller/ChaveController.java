ac4816f433869b8b891e481f647c153957ba9c6e
Andre Nunes <andre.nunes2@fatec.sp.gov.br>
2020-07-12 17:07:45 -0300

    Começo do controller de Chaves

--- Back-End/spring-test/src/main/java/br/com/torneio/gerenciador/controller/ChaveController.java
+++ Back-End/spring-test/src/main/java/br/com/torneio/gerenciador/controller/ChaveController.java
@@ -1,19 +1,37 @@
 package br.com.torneio.gerenciador.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.torneio.gerenciador.model.Atleta;
+import br.com.torneio.gerenciador.model.Organizador;
 import br.com.torneio.gerenciador.model.PartidaPK;
 import br.com.torneio.gerenciador.model.Torneio;
+import br.com.torneio.gerenciador.repository.TorneioRepository;
 
 @Controller
 @RequestMapping("/chave")
 public class ChaveController {
+	
+	@Autowired
+	private TorneioRepository tr;
+	
+	
+	
 
 	@GetMapping()
-	public List<ParidaPK> getParidas(Torneio torneio){
-		return; 
+	public ModelAndView getParidas(@PathVariable("id_torneio") long id_torneio){
+		ModelAndView mv = new ModelAndView("formChaves");
+		Optional<Torneio> torneio = tr.findById(id_torneio);
+        mv.addObject("torneio", torneio);          
+		
+		return mv;
 		
 	}
 	
